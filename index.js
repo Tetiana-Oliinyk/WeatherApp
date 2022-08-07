c0e61ae31a4a3beedeb9029ee2d3eb48f7c00508
Tetiana-Oliinyk <oliynuk72@gmail.com>
2022-08-07 03:57:27 +0300

    Moved my project to GitHub

---
+++ index.js
@@ -0,0 +1,63 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+
+let weatherDate = document.querySelector("#current-date");
+weatherDate.innerHTML = `${day}, ${date} ${month}`;
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let weatherTime = document.querySelector("#current-time");
+weatherTime.innerHTML = `${hours}:${minutes}`;
+
+function currentWeather(event) {
+  event.preventDefault();
+  let apiKey = "0394971ede805c240c5c5a180c481699";
+  let yourCity = document.querySelector("#inputCity").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${yourCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperatureCity);
+}
+
+function showTemperatureCity(response) {
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+let formCity = document.querySelector("#search-city");
+formCity.addEventListener("submit", currentWeather);
