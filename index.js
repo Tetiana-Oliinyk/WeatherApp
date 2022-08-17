9d18c7e34a18e92de06ecc282acfeb9a03721565
Tetiana-Oliinyk <oliynuk72@gmail.com>
2022-08-18 00:10:09 +0300

    added api-call for forecast

--- index.js
+++ index.js
@@ -40,12 +40,22 @@ if (minutes < 10) {
 let weatherTime = document.querySelector("#current-time");
 weatherTime.innerHTML = `${hours}:${minutes}`;
 
-function currentWeather(event) {
-  event.preventDefault();
+function currentWeather(city) {
   let apiKey = "0394971ede805c240c5c5a180c481699";
-  let yourCity = document.querySelector("#inputCity").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${yourCity}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperatureCity);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#inputCity");
+  currentWeather(cityInputElement.value);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "0394971ede805c240c5c5a180c481699";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperatureCity(response) {
@@ -64,12 +74,15 @@ function showTemperatureCity(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 let formCity = document.querySelector("#search-city");
-formCity.addEventListener("submit", currentWeather);
+formCity.addEventListener("submit", handleSubmit);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -92,4 +105,5 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
 }
-displayForecast();
+
+currentWeather("Kyiv");
